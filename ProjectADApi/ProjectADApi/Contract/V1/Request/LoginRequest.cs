e6c71705db0a3bb0989de9fdba9738778fa71f33
Bakare T. Daketima <teslimbakare@live.com>
2020-04-13 11:28:21 +0100

    Microsoft.AspNetCore.Http.Features installed on api.emailservice project.

--- ProjectADApi/ProjectADApi/Contract/V1/Request/LoginRequest.cs
+++ ProjectADApi/ProjectADApi/Contract/V1/Request/LoginRequest.cs
@@ -12,7 +12,7 @@ namespace ProjectADApi.Contract.V1.Request
         [EmailAddress]
         public string  username { get; set; }
 
-        [Required]
+       
         public string password { get; set; }
 
     }
